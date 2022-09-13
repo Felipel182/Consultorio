b88576480ec704714105460a34a57e0e0aa28b41
Rafa <rafa.qo@gmail.com>
2022-09-12 23:42:22 -0300

    Correção de bugs

--- src/main/java/com/example/Consultorio/service/ConsultaService.java
+++ src/main/java/com/example/Consultorio/service/ConsultaService.java
@@ -1,18 +1,18 @@
 package com.example.Consultorio.service;
 
 import com.example.Consultorio.model.ConsultaModel;
-import com.example.Consultorio.model.Dentista;
+import org.springframework.stereotype.Service;
 
 import java.sql.SQLException;
 import java.util.List;
-
+@Service
 public interface ConsultaService<T> {
 
-    public String salvar(ConsultaModel c) throws SQLException;
+    String salvar(ConsultaModel c) throws SQLException;
 
-    public List<ConsultaModel> buscarTodos() throws SQLException;
+    List<ConsultaModel> buscarTodos() throws SQLException;
 
-    public String updateConsulta(ConsultaModel c) throws SQLException;
+    String updateConsulta(ConsultaModel c) throws SQLException;
 
-    public String excluir(Integer id) throws SQLException;
+    String excluir(Integer id) throws SQLException;
 }
